e163dc6c2d48e851551cea40c42b83e520b46057
Administrator <18758714242@163.com>
2017-11-13 21:05:41 +0800

    环境搭建

---
+++ dd-manager/dd-manager-web/src/main/java/com/qf/novelwork/web/IndexAction.java
@@ -0,0 +1,24 @@
+package com.qf.novelwork.web;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by ${lxh} on 2017/11/13 0013
+ */
+@Controller
+@Scope("prototype")
+public class IndexAction {
+    @RequestMapping("/")
+    public String  index(){
+        return "index";
+    }
+
+    @RequestMapping("/{page}")
+    public String page(@PathVariable("page") String page){
+        return page;
+    }
+
+}
---
+++ dd-manager/dd-manager-web/src/main/webapp/WEB-INF/jsp/index.jsp
@@ -0,0 +1,12 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+         pageEncoding="UTF-8" %>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>首页</title>
+</head>
+<body>
+<h2>Hello 纵横千峰小说网</h2>
+</body>
+</html>
